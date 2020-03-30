e3cac56e2575b83ef633db63fe41c351258b6608
HJK <HJKdev@gmail.com>
2020-03-30 14:50:14 +0800

    ssh扫描器插件

---
+++ totalpwd/addons/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+
+from os.path import dirname, basename, isfile, join
+import glob
+
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [
+    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
+]
+
+from . import *
---
+++ totalpwd/addons/ssh.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+"""
+    SSH 扫描器
+"""
+
+import logging
+import paramiko
+from ..scanner import Scanner
+from ..settings import opts
+
+
+class SSHScanner(Scanner):
+    """
+        SSH 扫描器
+    """
+
+    def __init__(self, pwd, target, username, password):
+        super(SSHScanner, self).__init__(pwd, target, username, password)
+        self.port = self.port or 22
+
+    def _check(self):
+        c = paramiko.SSHClient()
+        c.set_missing_host_key_policy(
+            paramiko.MissingHostKeyPolicy()
+        )  # ignore unknown hosts
+        c.connect(
+            hostname=self.host,
+            port=self.port,
+            username=self.username,
+            password=self.password,
+            timeout=opts.timeout,
+            banner_timeout=200,
+        )
+        stdin, stdout, stderr = c.exec_command("uname -a")
+        evidence = stdout.readlines()[0]
+        c.close()
+        return evidence
+
+
+def mkscanners(pwds, targets) -> list:
+    logger = logging.getLogger("TotalPwd")
+    logger.info("Creating SSHScanners...")
+    scanners = []
+    for pwd in pwds:
+        # 如果不属于SSH Pwd或通用Pwd
+        if pwd.category != "ssh" and pwd.category != opts.common:
+            continue
+        for target in targets:
+            for cred in pwd.credentials:
+                scanners.append(
+                    SSHScanner(
+                        pwd, target, cred.get("username", ""), cred.get("password", "")
+                    )
+                )
+    return scanners
