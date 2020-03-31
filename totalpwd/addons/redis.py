387f3567eec35ec0ffce1663f4d1d527e6fb4726
HJK <HJKdev@gmail.com>
2020-03-31 16:11:46 +0800

    格式化

--- totalpwd/addons/redis.py
+++ totalpwd/addons/redis.py
@@ -9,21 +9,22 @@ import redis
 from ..settings import opts
 from ..scanner import Scanner
 
+
 class RedisScanner(Scanner):
     """
         Redis 密码扫描器
     """
+
     def __init__(self, pwd, target, username, password):
         super(RedisScanner, self).__init__(pwd, target, username, password)
         self.port = self.port or 6379
-    
+
     def _check(self):
         r = redis.StrictRedis(host=self.host, port=self.port)
         info = r.info()
-        evidence = "redis_version: %s, os: %s" % (info['redis_version'], info['os'])
+        evidence = "redis_version: %s, os: %s" % (info["redis_version"], info["os"])
         return evidence
 
 
 def mkscanner(pwd, target, username, password):
     return RedisScanner(pwd, target, username, password)
- 
